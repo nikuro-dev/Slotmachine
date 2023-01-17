f226d7d2096d7aafb90be95157e371f562ba4829
amirhossein <nikuro-dev>
2023-01-17 21:34:42 +0330

    fix variable problem & print rows in diffrent line

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ MAX_BET = 100
 MIN_BET = 1
 
 def spin_slot_machine(rows,cols,symbols):
-    symbols = []
+    all_symbols = []
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
-            symbols.append(symbol)
+            all_symbols.append(symbol)
     columns = []
     for _ in range(cols):
         column = []
-        current_symbols = symbols[:]
+        current_symbols = all_symbols[:]
         for _ in range(rows):
             value = random.choice(current_symbols)
             current_symbols.remove(value)
@@ -32,10 +32,10 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) -1:
-                print(column[row], "|")
+                print(column[row], end=" | ")
             else :
-                print(column[row])
-    
+                print(column[row],end=" ")
+        print()
 
 def deposit():
     while True:
@@ -90,7 +90,9 @@ def main():
             break
     
     print(f"you are betting ${bet} on {lines} lines. total bet amount is ${total_bet}")
-    
+
+    slots = spin_slot_machine(ROWS,COLS,symbol_count)
+    print_slot_machine(slots)
     
 if __name__ == "__main__":
     main()
