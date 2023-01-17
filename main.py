e8062c6b85b4cd93638e1d5c24eca469de1b49fa
amirhossein <nikuro-dev>
2023-01-17 21:21:24 +0330

    slot machine printer function

--- main.py
+++ main.py
@@ -28,7 +28,13 @@ def spin_slot_machine(rows,cols,symbols):
         columns.append(column)
     return columns
 
-
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) -1:
+                print(column[row], "|")
+            else :
+                print(column[row])
     
 
 def deposit():
